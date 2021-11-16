baf87434b6ba9bfd11467a6418509c4ef674532e
Name <Ku9ro6ha8@gmail.com>
2021-11-16 19:31:25 +0900

    ositara nannkaderu

--- Assets/Scripts/HomeScene.cs
+++ Assets/Scripts/HomeScene.cs
@@ -20,7 +20,13 @@ public class HomeScene : MonoBehaviour
     private GameObject _virusObj;
 
     [SerializeField]
-    private GameObject[] _spawner; 
+    private GameObject _spawner;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    private GameObject _parent;
+    private float repeat = 2f; // 何周期するか
+    float oneCycle = 2.0f * Mathf.PI; // sin の周期は 2π
     void Start()
     {
         var game = Game.Instance;
@@ -31,6 +37,21 @@ public class HomeScene : MonoBehaviour
         {
             var c = new Currency(_currencyType, _quantity);
             var cc = game.CashInventory.Add(c);
+            for (int i = 0; i < 5; i++)
+            {
+                GameObject copy = Instantiate(_virusObj, _parent.transform);
+                copy.transform.localPosition = _spawner.transform.localPosition;
+                var point = ((float)i / 5) * oneCycle; // 周期の位置 (1.0 = 100% の時 2π となる)
+                var repeatPoint = point * repeat; // 繰り返し位置
+
+                var x = Mathf.Cos(repeatPoint);
+                var y = Mathf.Sin(repeatPoint);
+
+                var position = new Vector3(x, y);
+                //var angles = new Vector3(0, 72 * i, 0);
+                //var direction = Quaternion.Euler(angles) * Vector3.forward;
+                copy.GetComponent<Rigidbody2D>().AddForce(position * speed, ForceMode2D.Impulse);
+            }
         });
         _scoreButton.onClick = e;
     }
